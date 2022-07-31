bec4144d3965b1d5b1267ccc3579ba4f7e9d3290
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-31 19:51:00 +0200

    added skills to home section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,44 @@
 import { motion } from "framer-motion";
+import { gsap } from "gsap";
+import { useRef } from "react";
 import { images } from "../../constants";
 import "./Home.scss";
 
 const Home = () => {
+  const circle = useRef<any>();
+  gsap.fromTo(
+    circle,
+    1.5,
+    { rotate: 0, scale: 0, x: 150, y: 50 },
+    { rotate: 360, scale: 1, x: 0, y: 0 }
+  );
+
   return (
     <div id="home" className="app__header app__flex app__primarybg">
       <div className="app__header-img">
-        <img src={images.profile} alt="" />
+        <img src={images.profile} alt="" className="bg_profile" />
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="overlay_circle"
+          className="bg_circle"
           src={images.circle}
           alt=""
         />
+        <div className="home__header-circles">
+          <div
+            className="home__header-circle"
+            id="home__header-circle-redux"
+            ref={circle}
+          >
+            <img src={images.redux} alt="" />
+          </div>
+          <div className="home__header-circle" id="home__header-circle-react">
+            <img src={images.react} alt="" />
+          </div>
+          <div className="home__header-circle" id="home__header-circle-node">
+            <img src={images.node} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,13 @@
 import profile from "../assets/profile3.png";
 import circle from "../assets/circle.svg";
+import redux from "../assets/redux.png";
+import react from "../assets/react.png";
+import node from "../assets/node.png";
 
 export default {
   profile,
   circle,
+  redux,
+  react,
+  node,
 };
