2f2e8a93745a7a3e4cfd470812ac38d999270008
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-08-02 19:56:28 +0200

    fixed styling of about me text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -28,8 +28,8 @@ const About = () => {
     gsap.set(ref.current, { opacity: 0, scale: 0.6 });
     gsap.to(ref.current, {
       scrollTrigger: {
-        trigger: ".myskills",
-        toggleActions: "restart pause reverse pause",
+        trigger: ".aboutme__skills-container",
+        toggleActions: "restart pause pause pause",
       },
       opacity: 1,
       scale: 1,
@@ -48,7 +48,27 @@ const About = () => {
 
   return (
     <motion.div id="about" className="app__wrapper app__primarybg">
-      <div className="myskills" ref={ref}>
+      <div className="aboutme__text-container" ref={ref}>
+        <p>
+          My name is <b>Piotr Rzadkowolski</b> and I have been fascinated and
+          involved in coding websites for some time.
+        </p>
+        <p>
+          I am a self-taught person and I derive my knowledge from people more
+          experienced in the industry. I currently live in Warsaw, and I focus
+          all my free time on developing my skills and learning about new
+          technologies that will make my projects even better.
+        </p>
+        <p>
+          I am a disciplined person, ready to take up work under stress and the
+          need to constantly improve my professional qualifications. I am
+          characterized by responsibility and the ability to work in a team, as
+          well as in independent positions. My advantage is high personal
+          culture, accuracy and innovation.
+        </p>
+        <p>I love diving, cycling and meeting new people and places.</p>
+      </div>
+      <div className="aboutme__skills-container" ref={ref}>
         {skillItem.map((skill, i) => (
           <motion.div className="aboutme__skills-item">
             <motion.div
@@ -62,27 +82,6 @@ const About = () => {
           </motion.div>
         ))}
       </div>
-
-      <p className="p-text">
-        My name is <b>Piotr Rzadkowolski</b> and I have been fascinated and
-        involved in coding websites for some time.
-      </p>
-      <p className="p-text">
-        I am a self-taught person and I derive my knowledge from people more
-        experienced in the industry. I currently live in Warsaw, and I focus all
-        my free time on developing my skills and learning about new technologies
-        that will make my projects even better.
-      </p>
-      <p className="p-text">
-        I am a disciplined person, ready to take up work under stress and the
-        need to constantly improve my professional qualifications. I am
-        characterized by responsibility and the ability to work in a team, as
-        well as in independent positions. My advantage is high personal culture,
-        accuracy and innovation.
-      </p>
-      <p className="p-text">
-        I love diving, cycling and meeting new people and places.
-      </p>
     </motion.div>
   );
 };
